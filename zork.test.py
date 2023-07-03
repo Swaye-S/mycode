f62b5e809a155a25438487caddaaa5c77461fe3e
Swaye-S <kwintin.cox@tlgcohort.com>
2023-07-03 23:02:28 +0000

    edited zork

--- zork.test.py
+++ zork.test.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 name = input('what is your name?>>>')
-day = input('what day of the week is it?>>>')
-print('Hello ' +name +'! '+ 'Happy ' +day +'!')
+day = input('what day of the week is it? >>>')
+age = input('How old are you? >>>')
+home = input('Where are you from? >>>')
+print('Hello ' +name+'! ''age '+age + ' Happy ' +day +'! ' 'Do you miss ' +home+'?')
