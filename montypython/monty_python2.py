902bf1b5290fa698bd153e2ad43c8ee1d49dc9d7
Swaye-S <kwintin.cox@tlgcohort.com>
2023-07-07 20:28:42 +0000

    studying for logic

---
+++ montypython/monty_python2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+round = 0
+while True:
+    round = round + 1
+    answer = input('Finish the movie title, "Monty Python\'s The Life of ______" >>>  ')
+    if answer == 'Brian':
+        print('Correct')
+        break
+    elif round==3:
+        print("Sorry, the answer was Brian.")
+        break
+    else:
+        print("Sorry! Try again!")
+
+# part two of template 
+#!/usr/bin/python3
+"""Alta3 Research | RZFeeser
+  Conditionals - Life of Brian guessing game without a while True loop."""
+
+round = 0
+answer = " "
+
+while round < 3 and answer != "Brian":
+    round += 1     # increase the round counter by 1
+    answer = input('Finish the movie title, "Monty Python\'s The Life of ______": ')
+    if answer == "Brian": # logic to check if user gave correct answer
+        print("Correct!")
+    elif round == 3:    # logic to ensure round has not yet reached 3
+        print("Sorry, the answer was Brian.")
+    else:                 # if answer was wrong
+        print("Sorry. Try again!")
+
